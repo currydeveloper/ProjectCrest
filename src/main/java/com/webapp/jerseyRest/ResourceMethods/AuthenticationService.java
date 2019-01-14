3587bd4c508b9b996105f6871be05652f2d5ed46
Krishna Sairam <currydeveloper@gmail.com>
2019-01-14 09:33:01 -0800

    1. Authentication method added which takes the credential string and
    returns true or false;

---
+++ src/main/java/com/webapp/jerseyRest/ResourceMethods/AuthenticationService.java
@@ -0,0 +1,28 @@
+package com.webapp.jerseyRest.ResourceMethods;
+
+import java.util.Base64;
+import java.util.StringTokenizer;
+
+public class AuthenticationService {
+	public boolean authenticate(String authCred) {
+		if (null != authCred || authCred.isEmpty()) {
+			return false;
+		} else {
+			System.out.println("Starting the authentication of data" + authCred);
+			final String encodedCred = authCred.replace("Basic", "");
+			String decodeUsn = null;
+			try {
+				byte[] decodeByte = Base64.getDecoder().decode(encodedCred);
+				decodeUsn = new String(decodeByte, "UTF-8");
+				System.out.println("decode value is" + decodeUsn);
+			} catch (Exception e) {
+				System.out.println("Error in decoding the usn" + e);
+			}
+			final StringTokenizer token = new StringTokenizer(decodeUsn, ":");
+			final String usn = token.nextToken();
+			final String pwd = token.nextToken();
+			boolean stat = "spadmin".equalsIgnoreCase(usn) && "admin".equalsIgnoreCase(pwd);
+			return stat;
+		}
+	}
+}
